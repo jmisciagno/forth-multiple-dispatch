90df1d6a297161fc690c3bde8b5976c4b71792c4
jmisciagno <johnmisciagno@gmail.com>
2022-04-22 12:00:57 -0400

    Update multi-dispatch.f

--- multi-dispatch.f
+++ multi-dispatch.f
@@ -30,25 +30,3 @@ variable null
 : >> ]] dup 0= if exit then >r [[ ; immediate
 : <<default>> :noname ]] -1 >r [[ ; 
 : ;; ]] r> ; [[ swap push ; immediate
-
-multi silly
-:: silly << dup 0= >> ." hi" cr ;;
-:: silly <<default>> ." bye" cr ;;
-
-0 silly
--1 silly 
-
-multi many
-:: many << dup 0 = >> ." A" cr ;;
-:: many << dup 1 = >> ." B" cr ;;
-:: many << dup 2 = >> ." C" cr ;;
-
-0 many
-1 many
-2 many
-
-multi factorial
-:: factorial << dup 0= >> drop 1 ;;
-:: factorial <<default>> dup 1- factorial * ;;
-
-5 factorial . cr
